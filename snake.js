5f035e4e9dfbd80d6095bba549ae9c202400b4f9
cir94 <claire.isabella.ramos@gmail.com>
2023-10-11 12:26:31 -0400

    Added rules such as game over state, restart state for when game over is reached

--- snake.js
+++ snake.js
@@ -29,8 +29,9 @@ export function expandSnake (amount) {
     newSegments += amount
 }
 
-export function onSnake(position) {
-    return snakeBody.some(segment => {
+export function onSnake(position, { ignoreHead = false } = {}) {
+    return snakeBody.some((segment, index) => {
+        if (ignoreHead && index == 0) return false
         return equalPositions(segment, position)
     })
 }
@@ -47,4 +48,12 @@ function addSegments() {
     ]
 
     newSegments = 0
+}
+
+export function getSnakeHead() {
+    return snakeBody[0]
+}
+
+export function snakeIntersection() {
+    return onSnake(snakeBody[0], { ignoreHead: true })
 }
